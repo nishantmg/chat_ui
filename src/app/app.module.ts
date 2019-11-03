6d9011a6ddf4a3bee0fcec921986ad900acc8abd
nishantmg <nishan.blkx@gmail.com>
2019-11-03 15:31:35 +0545

    basic chatting created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
 
+const config: SocketIoConfig = { url:'http://localhost:3001',options:{}} 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+      SocketIoModule.forRoot(config)],  
   providers: [
     StatusBar,
     SplashScreen,
